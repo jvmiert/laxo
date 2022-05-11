6513eee80ee53d21473c1398dfa3b8ac25e30c69
Jeroen <jvmiert@gmail.com>
2022-05-11 16:02:12 +0700

    fix removing entire notification icon when no notifications

--- frontend/components/DashboardNotificationControl.tsx
+++ frontend/components/DashboardNotificationControl.tsx
@@ -6,14 +6,14 @@ export default function DashboardNotificationControl() {
     useDashboard();
   return (
     <button type="button" onClick={toggleNotification}>
-      {!notificationLoading && dashboardState.notifications.length > 0 && (
-        <div className="relative mr-4 rounded-full bg-gray-100 p-1">
-          <BellIcon className="h-5 w-5 text-gray-900" />
+      <div className="relative mr-4 rounded-full bg-gray-100 p-1">
+        <BellIcon className="h-5 w-5 text-gray-900" />
+        {!notificationLoading && dashboardState.notifications.length > 0 && (
           <span className="text-light absolute top-0 right-0 inline-flex translate-x-1/2 -translate-y-1/2 transform items-center justify-center rounded-full bg-indigo-600 px-1.5 py-1 text-xs leading-none text-indigo-100">
             {dashboardState.notifications.length}
           </span>
-        </div>
-      )}
+        )}
+      </div>
     </button>
   );
 }
