b30c1e675dba02f930c59a8ed70b0c0609e0960c
Jeroen <jvmiert@gmail.com>
2022-05-12 11:25:32 +0700

    fix language menu not working

--- frontend/components/LangMenu.tsx
+++ frontend/components/LangMenu.tsx
@@ -1,45 +1,79 @@
+import { Fragment, forwardRef, ReactNode } from "react";
+import cc from "classcat";
 import { useRouter } from "next/router";
-import Link from "next/link";
-import { Popover } from "@headlessui/react";
+import Link, { LinkProps } from "next/link";
+import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/solid";
 import { GlobeAltIcon } from "@heroicons/react/outline";
+
+type EnhancedLinkProps = {
+  children: ReactNode[] | ReactNode;
+  className: string;
+} & LinkProps;
+
+const LangLink = forwardRef<HTMLAnchorElement, EnhancedLinkProps>(
+  (props, ref) => {
+    let { href, children, locale, ...rest } = props;
+    return (
+      <Link href={href} locale={locale}>
+        <a ref={ref} {...rest}>
+          {children}
+        </a>
+      </Link>
+    );
+  },
+);
+
+LangLink.displayName = "LangLink";
 
 export default function LangMenu() {
   const { pathname, locale } = useRouter();
   return (
-    <Popover className="relative inline-block text-left">
-      <div>
-        <Popover.Button>
-          <GlobeAltIcon className="inline h-5 w-5 text-gray-900" />
-          <ChevronDownIcon className="inline h-5 w-5 text-gray-900" />
-        </Popover.Button>
-      </div>
-      <Popover.Panel className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <div className="py-1">
-          <Popover.Button as="div">
-            <Link href={pathname} locale="en">
-              <a
-                className={`block px-4 py-2 text-sm ${
-                  locale == "en" ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                }`}
+    <Menu as="div" className="relative z-10 inline-block text-left">
+      <Menu.Button>
+        <GlobeAltIcon className="inline h-5 w-5 text-gray-900" />
+        <ChevronDownIcon className="inline h-5 w-5 text-gray-900" />
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              <LangLink
+                href={pathname}
+                locale="en"
+                className={cc([
+                  "block px-4 py-2 text-sm",
+                  { "bg-gray-100 text-gray-900": locale == "en" },
+                  { "text-gray-700": locale != "en" },
+                ])}
               >
                 English
-              </a>
-            </Link>
-          </Popover.Button>
-          <Popover.Button as="div">
-            <Link href={pathname} locale="vi">
-              <a
-                className={`block px-4 py-2 text-sm ${
-                  locale == "vi" ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                }`}
+              </LangLink>
+            </Menu.Item>
+            <Menu.Item>
+              <LangLink
+                href={pathname}
+                locale="vi"
+                className={cc([
+                  "block px-4 py-2 text-sm",
+                  { "bg-gray-100 text-gray-900": locale == "vi" },
+                  { "text-gray-700": locale != "vi" },
+                ])}
               >
                 Tiếng Việt
-              </a>
-            </Link>
-          </Popover.Button>
-        </div>
-      </Popover.Panel>
-    </Popover>
+              </LangLink>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   );
 }
