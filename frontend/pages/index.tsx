d9ce4f54ff22474e27fb86677a0130616ab8c19e
Jeroen <jvmiert@gmail.com>
2022-05-29 20:09:47 +0700

    do some work on landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import { useIntl } from "react-intl";
 import Head from "next/head";
 import DefaultLayout from "@/components/DefaultLayout";
 import { useAuth } from "@/providers/AuthProvider";
+import { AnnotationIcon } from "@heroicons/react/outline";
 
 export default function HomePage() {
   const t = useIntl();
@@ -14,13 +15,288 @@ export default function HomePage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <p className="text-3xl font-bold underline">
-        {t.formatMessage({
-          defaultMessage: "Hello World",
-          description: "Index Page: title",
-        })}
-      </p>
-      <p>You are {!auth && "not"} authenticated!</p>
+      <section>
+        <div className="mx-auto max-w-7xl px-4 pb-12 pt-6 sm:pb-6 sm:pt-3 md:px-12 lg:px-24">
+          <div className="mx-auto flex max-w-7xl flex-wrap items-center">
+            <div className="mt-12 mb-16 flex flex-col items-start text-left md:mb-0 lg:w-1/2 lg:flex-grow lg:pl-6 xl:mt-0 xl:pl-24">
+              <span className="mb-8 text-xs font-bold uppercase tracking-widest text-indigo-600">
+                Sell better, sell more.
+              </span>
+              <h1 className="mb-8 text-4xl font-bold leading-none tracking-tighter md:text-7xl lg:text-5xl">
+                A tool to make your selling experience easier
+              </h1>
+              <p className="mb-8 text-left text-base leading-relaxed text-gray-500">
+                Laxo helps you with managing your sales channels, your orders,
+                and your marketing activities
+              </p>
+              <div className="mt-0 max-w-7xl flex-col sm:flex lg:mt-6">
+                <form className="border2 mt-8 transform rounded-xl bg-gray-50 p-2 transition duration-500 ease-in-out sm:flex sm:max-w-lg">
+                  <div className="revue-form-group min-w-0 flex-1">
+                    <label htmlFor="email" className="sr-only">
+                      Email address
+                    </label>
+                    <input
+                      id="email"
+                      type="email"
+                      className="block w-full transform rounded-md border border-transparent bg-transparent px-5 py-3 text-base text-neutral-600 placeholder-gray-400 transition duration-500 ease-in-out focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                      placeholder="Enter your email  "
+                    />
+                  </div>
+                  <div className="mt-4 sm:mt-0 sm:ml-3">
+                    <button
+                      type="submit"
+                      value="Subscribe"
+                      className="w-full rounded-lg bg-indigo-500 py-3 px-5 font-bold text-white shadow-lg shadow-indigo-500/50 hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200 disabled:cursor-not-allowed disabled:bg-indigo-200 sm:px-10"
+                    >
+                      Notify me
+                    </button>
+                  </div>
+                </form>
+                <div className="sm:flex sm:max-w-lg">
+                  <p className="mt-3 text-xs text-gray-500">
+                    By subscribing, you agree with our
+                    <a target="_blank" rel="noreferrer" href="/terms">
+                      Terms of Service
+                    </a>
+                    and
+                    <a target="_blank" rel="noreferrer" href="/privacy">
+                      Privacy Policy
+                    </a>
+                    .
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="w-full rounded-xl lg:w-1/2 lg:max-w-lg">
+              <div>
+                <div className="relative w-full max-w-lg">
+                  <div className="absolute top-0 -right-4 h-72 w-72 rounded-full bg-violet-300 opacity-70 mix-blend-multiply blur-xl filter"></div>
+
+                  <div className="absolute left-20 h-72 w-72 rounded-full bg-fuchsia-300 opacity-70 mix-blend-multiply blur-xl filter lg:-bottom-24"></div>
+                  <div className="relative"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+          <div className="relative">
+            <h3 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+              Sell more by selling everywhere
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              Stop selecting your sales channels based on your available
+              resources. Manage your products on Laxo and let us take care of
+              the rest.
+            </p>
+
+            <ul className="mt-10 space-y-10">
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Update your product photos
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Change your product descriptions
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Adjust your product prices
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
+            <svg
+              className="relative mx-auto"
+              xmlns="http://www.w3.org/2000/svg"
+              width="490"
+              height="570"
+              viewBox="0 0 490 570"
+            >
+              <rect fill="#ddd" width="490" height="570" />
+              <text
+                fill="rgba(0,0,0,0.5)"
+                fontFamily="sans-serif"
+                fontSize="30"
+                dy="10.5"
+                fontWeight="bold"
+                x="50%"
+                y="50%"
+                textAnchor="middle"
+              >
+                ...
+              </text>
+            </svg>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center lg:gap-8">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+                Conveniently manage your{" "}
+                <span className="whitespace-nowrap">orders & inventory</span>
+              </h3>
+              <p className="mt-3 text-lg text-gray-500">
+                Laxo keeps track of all your sales channels to allow you to
+                track, review, and manage everything in one place.
+              </p>
+
+              <ul className="mt-10 space-y-10">
+                <li>
+                  <div className="flex items-center">
+                    <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                      <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="ml-8 text-lg font-medium text-gray-900">
+                      See all your sales in one place
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                      <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="ml-8 text-lg font-medium text-gray-900">
+                      Manage your inventory in one place
+                    </p>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                      <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="ml-8 text-lg font-medium text-gray-900">
+                      Fullfill your orders and returns in once place
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+
+            <div className="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
+              <svg
+                className="relative mx-auto"
+                xmlns="http://www.w3.org/2000/svg"
+                width="490"
+                height="570"
+                viewBox="0 0 490 570"
+              >
+                <rect fill="#ddd" width="490" height="570" />
+                <text
+                  fill="rgba(0,0,0,0.5)"
+                  fontFamily="sans-serif"
+                  fontSize="30"
+                  dy="10.5"
+                  fontWeight="bold"
+                  x="50%"
+                  y="50%"
+                  textAnchor="middle"
+                >
+                  ...
+                </text>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+          <div className="relative">
+            <h3 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">
+              Create coordinated marketing campaigns
+            </h3>
+            <p className="mt-3 text-lg text-gray-500">
+              Design and create your marketing campaign simply on Laxo and apply
+              to your sales channels all at once.
+            </p>
+
+            <ul className="mt-10 space-y-10">
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Easily create special product image frames
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Easily apply special promotion prices
+                  </p>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <div className="flex h-12 w-12 items-center justify-center rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-8 text-lg font-medium text-gray-900">
+                    Easily distribute discount coupons
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
+            <svg
+              className="relative mx-auto"
+              xmlns="http://www.w3.org/2000/svg"
+              width="490"
+              height="570"
+              viewBox="0 0 490 570"
+            >
+              <rect fill="#ddd" width="490" height="570" />
+              <text
+                fill="rgba(0,0,0,0.5)"
+                fontFamily="sans-serif"
+                fontSize="30"
+                dy="10.5"
+                fontWeight="bold"
+                x="50%"
+                y="50%"
+                textAnchor="middle"
+              >
+                ...
+              </text>
+            </svg>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- landingpage.md
+++ landingpage.md
@@ -1,10 +1,50 @@
-# Section 1 - Introduction
-Laxo - help you sell easier, better, more
+# Section 1 - Header
+Laxo - Sell better, sell more.
 
-By: keep track of your stocks, making it easier to run marketing campaigns, and managing your orders
+A tool to make your selling experience easier
+
+Laxo helps you with managing your sales channels, your orders, and your marketing activities
+Receive all you orders, manage your entire inventory, create your marketing campaigns - all from one place!
+
+
+
+
+
+
+
+
+Laxo takes care of the difficult tasks.
+Laxo allows you to utilize more sales channels than you can handle now.
+Laxo helps you manage more sales channels with less resources.
+Laxo automates the boring tasks so you can focus on selling
+
+
+# Section 1 - Sell more by selling everywhere
+Stop selecting your sales channels based on your available resources. Manage your products on Laxo and let us take care of the rest.
+
+- Update your product photos
+- Change your product descriptions
+- Adjust your product prices
+
+# Section 2 - Conveniently manage your orders & inventory
+Laxo keeps track of all your sales channels to allow you to track, review, and manage everything in one place.
+
+- See all your sales in one place
+- Manage your inventory in one place
+- Fullfill your orders and returns in once place
+
+# Section 3 - Create coordinated marketing campaigns
+Design and create your marketing campaign simply on Laxo and apply to your sales channels all at once.
+
+- Easily create special product image frames
+- Easily apply special promotion prices
+- Easily distribute discount coupons
+
+
+
+
+
+
 
 ## Graphic
 I want to create a graphic where there's a central graphic representing laxo. This central graphic will illustrate, in animation, the above "By" section. Then, below or to the side of the laxo representation, there will be 3 branches that illustrate the synchronization between other platforms (shopee, lazada, tiki)
-
-# Section 2 - Call to action
-Coming soon - input your email address for getting notified.
